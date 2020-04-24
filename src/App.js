580b5528aa69ad600a87dd7aa5bb8b783139925c
yaelahky <kiky.lenovo@gmail.com>
2020-04-24 15:45:14 +0700

    add logic to check cors

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    rendering: false,
+    url: ''
+  }
+
+  handleRequest = (e) => {
+    e.preventDefault()
+
+    Axios.get(this.state.url)
+      .then(res => {
+        this.renderRes(res.data.toString())
+        console.log("Berhasil")
+      })
+      .catch(err => {
+        this.renderRes(err)
+        console.log("Gagal")
+      })
+  }
+
+  renderRes = (data) => (
+    <p> {data || ""} </p>
+  )
+
+  render() {
+    return (
+      <div className="App">
+        <p>CHECK YOUR CORS</p>
+        <form>
+          <label>GET:</label>
+          <input type="text" onChange={(e) => {this.setState({url: e.target.value})}} style={{width: 200}} placeholder="http://localhost:3001/products" />
+          <button onClick={(e) => this.handleRequest(e)}>CHECK</button>
+        </form>
+        {this.renderRes()}
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
